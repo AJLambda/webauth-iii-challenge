1d4b69ff9e9226787edccfa48315281bd04f62bf
AJLambda <apj2277@gmail.com>
2019-05-08 15:28:05 -0700

    MVP section 1 complete

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
 const parser = express.json();
@@ -10,6 +11,9 @@ const parser = express.json();
 server.use(helmet());
 server.use(parser);
 server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send("It's alive!");
